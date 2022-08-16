8ea9b59a45b0b10e0e670630c6805bd7393b612a
Gennady Guniashev <68774508+Genateep@users.noreply.github.com>
2022-08-16 16:52:07 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# autotester
+
+- Ссылка на скачивание игры http://www.ag.ru/games/kkrieger/demos# (Альтернативная ссылка: http://web.archive.org/web/20110717024227/http://www.theprodukkt.com/kkrieger#20) В случае проблем с запуском приложения, установите режим совместимости с Windows XP (Пакет обновления 2), необходим DX9.
+- перед использованием скачать и запустить программу считывания FPS: https://fraps.com/download.php установив галочку на FPS в Benchmark settings (по умолчанию в "C:\Fraps")
+
+Пример использования (команды опциональны):
+```
+main.py <path\to\kkrieger> -o <path\to\output> 
+```
---
+++ main.py
@@ -0,0 +1,108 @@
+import argparse
+import csv
+import glob
+import pyautogui as pag
+import subprocess as s
+import os
+
+from datetime import datetime
+from pathlib import Path
+from time import sleep
+
+parser = argparse.ArgumentParser(description='kkrieger autotest')
+parser.add_argument(
+    'file',
+    nargs='?',
+    default='pno0001.exe',
+    help='Path to file, default = current dir'
+)
+parser.add_argument(
+    '-o',
+    default=Path.cwd() / f'results_{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}',
+    help='Path to output, default = current dir'
+)
+args = parser.parse_args()
+
+fraps_log_dir = 'C:/Fraps/Benchmarks'
+
+
+def press_key(key: str, interval=0.0, times=0):
+    for _ in range(times + 1):
+        pag.keyDown(key)
+        sleep(interval)
+        pag.keyUp(key)
+        sleep(1)
+
+
+def click(interval=0, times=0):
+    for _ in range(times + 1):
+        pag.mouseDown(button='left')
+        sleep(interval)
+        pag.mouseUp(button='left')
+        sleep(0.5)
+
+
+def main():
+    # checking output dir
+    if not Path(args.o).exists():
+        os.mkdir(args.o)
+        print(f'{args.o} folder created')
+
+    # launching process
+    start_time = datetime.now()
+    p = s.Popen([args.file, '-f'])
+    print('Loading game')
+    sleep(3)
+
+    # waiting for game to load by checking background color
+    while pag.pixelMatchesColor(300, 300, (0, 0, 0)):
+        sleep(1)
+
+    # skipping intro and starting game
+    press_key('enter', times=2)
+    print(f'Game loaded in {datetime.now() - start_time}')
+
+    # making start_screenshot
+    scr = pag.screenshot()
+    scr.save(Path(args.o) / 'scr_start.png')
+
+    # start collecting statistics
+    press_key('f11')
+
+    # shoot on target and move forward
+    pag.moveRel(-3, 6, 1)
+    click(times=3)
+    press_key('w', interval=10)
+
+    # stop collecting statistics
+    press_key('f11')
+
+    # making end_screenshot
+    scr = pag.screenshot()
+    scr.save(Path(args.o) / 'scr_end.png')
+
+    # terminate process
+    p.kill()
+    print(f'Scenario finished in {datetime.now() - start_time}')
+
+    # getting last benchmark file and creating new with average fps number
+    list_of_files = glob.glob(fraps_log_dir + '/*.csv')
+    latest_file = max(list_of_files, key=os.path.getctime).replace('\\', '/')
+    fps_list = []
+
+    with open(latest_file, 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in reader:
+            fps_list.append(row[0])
+
+    fps_list.pop(0)
+    fps_average = round(sum([int(x) for x in fps_list]) / len(fps_list))
+
+    with open(Path(args.o) / 'average_fps.txt', 'w') as file:
+        file.write(str(fps_average))
+
+    print('average_fps.txt created')
+
+
+if __name__ == '__main__':
+    main()
